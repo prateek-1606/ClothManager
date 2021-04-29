3234b7f9f25ae9abd1d7ef0ae3f512aae35e4666
Prateek Varshney <varshneyprateek20@gmail.com>
2021-04-29 13:44:27 +0530

    first commit

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGOURI: 'mongodb+srv://Prateek:Prateek123@cluster0.uwl72.mongodb.net/CLOTHMANAGER?retryWrites=true&w=majority',
-    JWT_SECRET: 'Prateek123',
-    PORT: 5000
-}
